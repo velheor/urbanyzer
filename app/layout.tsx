17842b4dcd198f7feeeb9ae8f440c6a2075dbe64
velheor <vlad.atsman@gmail.com>
2025-06-25 11:51:03 +0300

    Hotfix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,53 +1,17 @@
 import * as React from 'react';
 import { NextAppProvider } from '@toolpad/core/nextjs';
-import type { Navigation } from '@toolpad/core/AppProvider';
-import { SessionProvider, signIn, signOut } from 'next-auth/react';
 import theme from '../theme';
 
-const NAVIGATION: Navigation = [
-  // {
-  //   kind: 'header',
-  //   title: 'Main items',
-  // },
-  // {
-    // title: 'Dashboard',
-    // icon: <DashboardIcon />,
-  // },
-  // {
-  //   segment: 'orders',
-  //   title: 'Orders',
-  //   icon: <ShoppingCartIcon />,
-  // },
-  // {
-  //   segment: 'employees',
-  //   title: 'Employees',
-  //   icon: <PersonIcon />,
-  //   pattern: 'employees{/:employeeId}*',
-  // },
-];
-
-const AUTHENTICATION = {
-  signIn,
-  signOut,
-};
-
-export default async function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return (
-    <html lang="ru" data-toolpad-color-scheme="light">
-      <body>
-        {/*<SessionProvider session={session}>*/}
-        {/*  <AppRouterCacheProvider options={{ enableCssLayer: true }}>*/}
-            <NextAppProvider
-              theme={theme}
-              // navigation={NAVIGATION}
-              // session={session}
-              // authentication={AUTHENTICATION}
-            >
-              {children}
-            </NextAppProvider>
-          {/*</AppRouterCacheProvider>*/}
-        {/*</SessionProvider>*/}
-      </body>
-    </html>
-  );
+export default async function RootLayout({children}: Readonly<{ children: React.ReactNode }>) {
+    return (
+        <html lang="ru" data-toolpad-color-scheme="light">
+        <body>
+        <NextAppProvider
+            theme={theme}
+        >
+            {children}
+        </NextAppProvider>
+        </body>
+        </html>
+    );
 }
